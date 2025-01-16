785293516e7f2c64710fa3a1dcb9f755d3b9ab5f
Khalid Belhadj <khalidbelhadj124@gmail.com>
2025-01-16 16:35:23 +0000

    Use custom json encoder

--- honours_project/data.py
+++ honours_project/data.py
@@ -1,6 +1,12 @@
+# type: ignore
 from dataclasses import dataclass
+
+import dataclasses
+from abc import ABC
+
 import json
-from typing import NamedTuple
+
+import numpy as np
 
 
 @dataclass(frozen=True)
@@ -14,7 +20,7 @@ class Allocation:
 
 
 @dataclass(frozen=True)
-class SimulationResult:
+class SimulationResult(ABC):
     time: float
     iteration: int
 
@@ -30,7 +36,7 @@ class PNE(SimulationResult):
 
 
 @dataclass(frozen=True)
-class FPAResult:
+class FPAResult(ABC):
     pass
 
 
@@ -50,3 +56,32 @@ class BestResponse:
     new_alpha: float
     new_utility: float
     old_utility: float
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o: object) -> object:
+        # Dataclass serialisation
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+
+        # Numpy serialisation
+        if isinstance(o, (np.bool_)):
+            return bool(o)
+        elif isinstance(
+            o, (np.int_, np.intc, np.intp, np.int8, np.int16, np.int32, np.int64)
+        ):
+            return int(o)
+        elif isinstance(o, (np.uint8, np.uint16, np.uint32, np.uint64)):
+            return int(o)
+        elif isinstance(o, (np.float_, np.float16, np.float32, np.float64)):
+            return float(o)
+        elif isinstance(o, (np.complex_, np.complex64, np.complex128)):
+            return {"real": o.real, "imag": o.imag}
+        elif isinstance(o, (np.ndarray,)):
+            return o.tolist()
+        elif isinstance(o, (np.bool_)):
+            return bool(o)
+        elif isinstance(o, (np.void)):
+            return None
+
+        return super().default(o)
